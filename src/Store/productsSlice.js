2a0545fb8a0227257c0b4a76737c847cfe2257a9
vixifying <vikramofficial@protonmail.com>
2024-01-02 20:37:50 +0530

    Minor Fix

--- src/Store/productsSlice.js
+++ src/Store/productsSlice.js
@@ -15,7 +15,7 @@ export const { fetchProducts } = productSlice.actions;
 export default productSlice.reducer;
 
 export function getProducts() {
-  return async function getProductsThunk(dispatch, getState) {
+  return async function getProductsThunk(dispatch) {
     const data = await fetch("https://fakestoreapi.com/products");
     const res = await data.json();
     dispatch(fetchProducts(res));
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../Store/productsSlice";
 import { addToCart } from "../Store/cartSlice";
